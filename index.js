a31e0a4870854dfa1a02f7f375071f1f6b2eae64
SkAbid-a2i <sk.abid@a2i.gov.bd>
2025-08-26 12:32:07 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => res.send('D-Nothi API is running'));
+
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/tasks', require('./routes/tasks'));
+app.use('/api/leaves', require('./routes/leaves'));
+app.use('/api/dropdowns', require('./routes/dropdowns'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
